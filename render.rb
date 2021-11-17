5ece619bf18796d732974d8f0bb79c1b88ee43db
Your Name <your.email@example.com>
2021-11-17 19:46:08 +0800

    split each post into a feed
    Former-commit-id: 14bd8e66b7fed7f942b52c656feb80cabf1c3553

--- render.rb
+++ render.rb
@@ -3,13 +3,21 @@ require 'json'
 def start
   rss_items_file = './rss_items.json'
   rss_items = JSON.parse(File.open(rss_items_file).read)
-  rss_items.each do |rss_item|
-    render_rss_item(rss_item)
+  rss_items.each_with_index do |rss_item, index|
+    render_rss_item(rss_item, index)
   end
 end
 
-def render_rss_item(rss_item)
+def render_rss_item(rss_item, index)
 result = <<-ITEM
+<?xml version="1.0" encoding="utf-8"?>
+<rss version="2.0">
+<channel>
+<title>Coding Horror</title>
+<description>programming and human factors</description>
+<link>https://blog.codinghorror.com/</link>
+<pubDate>Sun, 19 Apr 2020 00:00:01 GMT</pubDate>
+<!-- other elements omitted from this example -->
 <item>
 <title>#{rss_item["title"]}</title>
 <link>#{rss_item["link"]}</link>
@@ -17,9 +25,11 @@ result = <<-ITEM
 <pubDate>#{rss_item["published_date"]}</pubDate>
 <guid>#{rss_item["link"]}</guid>
 </item>
+</channel>
+</rss>
 ITEM
 
-  puts result
+  File.open("./out-#{index}.xml", 'w') { |file| file.write(result) }
 end
 
 start
