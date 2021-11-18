0e7032b73f9027d03c15e664a61060442c168abc
Shunfa Xu <gouwa5700@gmail.com>
2021-11-18 10:02:44 +0800

    split posts into slices
    Former-commit-id: 6bb7b0d10d681b6fb17802b622cca0d3c23a8197

--- render.rb
+++ render.rb
@@ -3,12 +3,26 @@ require 'json'
 def start
   rss_items_file = './rss_items.json'
   rss_items = JSON.parse(File.open(rss_items_file).read)
-  rss_items.each_with_index do |rss_item, index|
-    render_rss_item(rss_item, index)
+  rss_items.sort_by { |hs| hs["published_date"] }.reverse.each_slice(14).with_index do |slice, index|
+    rss_content = ''
+    slice.each do |rss_item|
+      rss_content.concat(render_rss_item(rss_item))
+    end
+    rss_slice_feed = render_rss_header + rss_content + render_rss_footer
+    File.open("./out/feeds.txt", 'a') { |file| file.write("https://raw.githubusercontent.com/goooooouwa/htmlparser/master/out/slice-#{index}.xml\n") }
+    File.open("./out/slice-#{index}.xml", 'w') { |file| file.write(rss_slice_feed) }
   end
 end
 
-def render_rss_item(rss_item, index)
+def render_rss_footer
+  <<-FOOTER
+</channel>
+</rss>
+FOOTER
+end
+
+def render_rss_header
+  <<-HEADER
 result = <<-ITEM
 <?xml version="1.0" encoding="utf-8"?>
 <rss version="2.0">
@@ -18,6 +32,11 @@ result = <<-ITEM
 <link>https://blog.codinghorror.com/</link>
 <pubDate>Sun, 19 Apr 2020 00:00:01 GMT</pubDate>
 <!-- other elements omitted from this example -->
+HEADER
+end
+
+def render_rss_item(rss_item)
+<<-ITEM
 <item>
 <title>#{rss_item["title"]}</title>
 <link>#{rss_item["link"]}</link>
@@ -25,12 +44,7 @@ result = <<-ITEM
 <pubDate>#{rss_item["published_date"]}</pubDate>
 <guid>#{rss_item["link"]}</guid>
 </item>
-</channel>
-</rss>
 ITEM
-
-  File.open("./out/feeds.txt", 'a') { |file| file.write("https://raw.githubusercontent.com/goooooouwa/htmlparser/master/out/out-#{index}.xml\n") }
-  File.open("./out/out-#{index}.xml", 'w') { |file| file.write(result) }
 end
 
 start
