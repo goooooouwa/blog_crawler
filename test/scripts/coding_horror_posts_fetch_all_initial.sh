eead8601d1c7c172b90f37ab3af78f652a11be4f
Shunfa Xu <gouwa5700@gmail.com>
2021-11-20 14:19:17 +0800

    add test scripts for coding horror

---
+++ test/scripts/coding_horror_posts_fetch_all_initial.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export PAGE_BASE_URL=https://blog.codinghorror.com
+export MAX_RETRY_COUNT=5
+export LOG_FILE=./fetch.log
+
+echo "[]" > ./out/test_coding_horror_posts.json
+
+ruby ./bin/fetch_all.rb CodingHorrorPost ./test/test_data/coding_horror_pages.json ./out/test_coding_horror_posts.json
