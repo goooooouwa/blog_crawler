beabb17c8a1de991e5716b580a49d937bf206380
Shunfa Xu <gouwa5700@gmail.com>
2021-11-20 14:29:35 +0800

    render blog

--- src/render.rb
+++ src/render.rb
@@ -1,12 +1,12 @@
 require 'json'
+require 'date'
 
-def start
-  rss_items_file = ENV["RSS_ITEMS_FILE"]
-  rss_items = JSON.parse(File.open(rss_items_file).read)
-  rss_items.sort_by { |hs| hs["published_date"] }.each_slice(ENV["SLICE"].to_i).with_index do |slice, index|
+def start(posts_file='./out/posts.json')
+  posts = JSON.parse(File.open(posts_file).read)
+  posts.sort_by { |hs| hs["published_date"] }.each_slice(ENV["SLICE"].to_i).with_index do |slice, index|
     rss_content = ''
-    slice.each do |rss_item|
-      rss_content.concat(render_rss_item_with_no_image(rss_item))
+    slice.each do |post|
+      rss_content.concat(render_rss_item_with_no_image(post))
     end
     rss_slice_feed = render_rss_header + rss_content + render_rss_footer
     File.open("#{ENV['OUT_DIR']}/feeds.txt", 'a') { |file| file.write("#{ENV['REMOTE_BASE_URL']}/slice-#{index}.xml\n") }
@@ -26,10 +26,10 @@ def render_rss_header
 <?xml version="1.0" encoding="utf-8"?>
 <rss version="2.0">
 <channel>
-<title>Coding Horror</title>
-<description>programming and human factors</description>
-<link>https://blog.codinghorror.com/</link>
-<pubDate>Sun, 19 Apr 2020 00:00:01 GMT</pubDate>
+<title>#{ENV['BLOG_TITLE']}</title>
+<description>#{ENV['BLOG_DESCRIPTION']}</description>
+<link>#{ENV['BLOG_BASE_URL']}</link>
+<pubDate>#{DateTime.now}</pubDate>
 <!-- other elements omitted from this example -->
 HEADER
 end
@@ -38,11 +38,11 @@ def render_rss_item_with_no_image(rss_item)
 <<-ITEM
 <item>
 <title><![CDATA[ #{rss_item["title"]} ]]></title>
-<link>#{rss_item["link"]}</link>
+<link>#{rss_item["page_link"]}</link>
 <content><![CDATA[ #{rss_item["content"].gsub(/<img.*>/, '<img alt="image placeholder" >')} ]]></content>
 <pubDate>#{rss_item["published_date"]}</pubDate>
-<guid>#{rss_item["link"]}</guid>
-<author><![CDATA[ Jeff Atwood ]]></author>
+<guid>#{rss_item["page_link"]}</guid>
+<author><![CDATA[ #{rss_item["author"]} ]]></author>
 </item>
 ITEM
 end
