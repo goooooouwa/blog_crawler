a14ee1c91470628f8c690f9bcccd7a7f96993b9b
Shunfa Xu <gouwa5700@gmail.com>
2021-11-20 16:36:34 +0800

    fix coolshell published date

--- src/page_objects/coolshell_post.rb
+++ src/page_objects/coolshell_post.rb
@@ -6,7 +6,7 @@ class CoolshellPost < Post
   def initialize(post_link, post_html)
     @page_link = post_link
     @title = post_html.css(".entry-title").text
-    @published_date = post_html.css(".entry-date").first.attributes["datetime"].value
+    @published_date = post_html.css(".entry-date .entry-date").first.attributes["datetime"].value
     @content = post_html.css(".entry-content").children
     @author = "陈皓"
   end
