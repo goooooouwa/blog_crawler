2b8fa8adea491d9f87e013c25c408039ee4459f7
Shunfa Xu <gouwa5700@gmail.com>
2021-12-06 19:26:35 +0800

    set and reload config

--- bin/fetch_all.rb
+++ bin/fetch_all.rb
@@ -4,5 +4,7 @@ require 'dotenv/load'
 require File.expand_path("../../src/config", __FILE__)
 Dir[File.join(File.dirname(__FILE__), "../src/blogs/*/*.rb")].each {|file| require file }
 
+Config.blog=(ARGV[1]) unless ARGV[1].nil?
+
 klass = Object.const_get(ARGV[0])
 klass.start(Config.blog)
--- src/config.rb
+++ src/config.rb
@@ -1,7 +1,18 @@
 require 'json'
 
 module Config
+
+  @@blog = JSON.parse(File.read(ENV['BLOG_CONFIG']))
+
   def self.blog
-    JSON.parse(File.read(ENV['BLOG_CONFIG']))
+    @@blog
+  end
+
+  def self.blog=(blog_config)
+    @@blog = JSON.parse(File.read(blog_config))
+  end
+
+  def self.reload!
+    @@blog = JSON.parse(File.read(ENV['BLOG_CONFIG']))
   end
 end
