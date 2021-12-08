7361c2394a71052cef01fca6643641091b23c6d7
Shunfa Xu <gouwa5700@gmail.com>
2021-12-08 10:32:30 +0800

    detect mp3 download link

--- src/blogs/white_house_press_briefings/white_house_press_briefings_post.rb
+++ src/blogs/white_house_press_briefings/white_house_press_briefings_post.rb
@@ -7,7 +7,8 @@ class WhiteHousePressBriefingsPost < Post
     super(post_link, page_html)
     @title = post_html.css("#media-detail #video-info .caption h1").text
     @published_date = DateTime.parse(post_link.match(/[0-9]{4}\/[0-9]{2}\/[0-9]{2}/)[0])
-    @content = post_html.css("#media-detail #video-info .download a")[1].attributes["href"].value
+    mp3_download_link = post_html.css("#media-detail #video-info .download a").detect {|a| a.attributes["href"].value.include? "mp3" }
+    @content = mp3_download_link.nil? ? nil : mp3_download_link.attributes["href"].value
     @author = "the White House"
   end
 end
