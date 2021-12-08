1b8f018687b2f7754bcbee0743c348948f9bf417
Shunfa Xu <gouwa5700@gmail.com>
2021-12-08 10:46:38 +0800

    use more stable published_date parser

--- src/blogs/white_house_press_briefings/white_house_press_briefings_post.rb
+++ src/blogs/white_house_press_briefings/white_house_press_briefings_post.rb
@@ -6,8 +6,8 @@ class WhiteHousePressBriefingsPost < Post
   def initialize(post_link, post_html)
     super(post_link, page_html)
     @title = post_html.css("#media-detail #video-info .caption h1").text
-    @published_date = DateTime.parse(post_link.match(/[0-9]{4}\/[0-9]{2}\/[0-9]{2}/)[0])
-    mp3_download_link = post_html.css("#media-detail #video-info .download a").detect {|a| a.attributes["href"].value.include? "mp3" }
+    @published_date = DateTime.parse(post_html.css("#media-detail #video-info .date-display-single").text)
+    mp3_download_link = post_html.css("#media-detail #video-info .download a").detect { |a| a.attributes["href"].value.include? "mp3" }
     @content = mp3_download_link.nil? ? nil : mp3_download_link.attributes["href"].value
     @author = "the White House"
   end
