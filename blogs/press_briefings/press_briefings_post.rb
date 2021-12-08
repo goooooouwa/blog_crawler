ef3028b237faaf21caefdba5ab16874f1fdabfc3
Shunfa Xu <gouwa5700@gmail.com>
2021-12-08 17:59:18 +0800

    add fallback css selector for published_date

--- blogs/press_briefings/press_briefings_post.rb
+++ blogs/press_briefings/press_briefings_post.rb
@@ -6,7 +6,11 @@ class PressBriefingsPost < Post
   def initialize(post_url, post_html)
     super(post_url, post_html)
     @title = post_html.css("#media-detail #video-info .caption h1").text
-    @published_date = DateTime.parse(post_html.css("#media-detail #video-info .caption .notes").text)
+    begin
+      @published_date = DateTime.parse(post_html.css("#media-detail #video-info .caption .notes").text)
+    rescue
+      @published_date = DateTime.parse(post_url.match(/[0-9]{4}\/[0-9]{2}\/[0-9]{2}/)[0])
+    end
     mp3_download_link = post_html.css("#media-detail #video-info .download a").detect { |a| a.attributes["href"].value.include? "mp3" }
     @content = mp3_download_link.nil? ? nil : mp3_download_link.attributes["href"].value
     @author = "the White House"
