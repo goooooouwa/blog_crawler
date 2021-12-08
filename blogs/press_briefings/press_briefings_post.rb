4ad386e5d51e37025d9728b45901e634744cf052
Shunfa Xu <gouwa5700@gmail.com>
2021-12-08 17:48:12 +0800

    published_date css selector: handle no date class

--- blogs/press_briefings/press_briefings_post.rb
+++ blogs/press_briefings/press_briefings_post.rb
@@ -6,7 +6,7 @@ class PressBriefingsPost < Post
   def initialize(post_url, post_html)
     super(post_url, post_html)
     @title = post_html.css("#media-detail #video-info .caption h1").text
-    @published_datese = DateTime.parse(post_html.css("#media-detail #video-info .date-display-single").text)
+    @published_date = DateTime.parse(post_html.css("#media-detail #video-info .caption .notes").text)
     mp3_download_link = post_html.css("#media-detail #video-info .download a").detect { |a| a.attributes["href"].value.include? "mp3" }
     @content = mp3_download_link.nil? ? nil : mp3_download_link.attributes["href"].value
     @author = "the White House"
